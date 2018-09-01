deaa9979044d42f45e93f7f07e592f843ef9d009
kajah90 <kjselnes@gmail.com>
2018-09-01 10:16:59 +0200

    added modal to start page, for starting new chat, also added a small line in the postMessage function,  to clear the text area after submitting

--- CommClient/public_html/clientJS.js
+++ CommClient/public_html/clientJS.js
@@ -5,10 +5,23 @@
  */
 
 var screen;
+var allChats = new Array();
+var aChat;
 
-function postMessage(message) {
+
+createChat("Per, Kurt");
+function createChat(chatMembers) {
+    var names = document.createElement("p");
+    names.style.wordWrap = "break-word";
+    names.innerHTML = "Chatting with: " + chatMembers;
+    screen = document.getElementById("chat");
+    console.log(names);
+    screen.insertBefore(names, screen.firstChild);
+}
+function postMessage(messageValue) {
     
     //TODO later, send message to server
+    var message = document.getElementById(messageValue).value;
     var post = document.createElement("p");
     post.id = 'chatMessage';
     post.style.wordWrap = "break-word";
@@ -19,6 +32,8 @@ function postMessage(message) {
     //but then the elements were added at the top of the screen, 
     //due to reversal, so i had to fix that here, with insertBefore instead of appendChild
     screen.insertBefore(post, screen.firstChild);
+    //clear text area after submit
+    document.getElementById(messageValue).value = "";
     //screen.appendChild(post); 
   
 }
